57acefb1091d7e454659ba1dacf171be45441c92
OleksandrRizun <137624723+OleksandrRizun@users.noreply.github.com>
2024-06-28 01:39:00 +0300

    second

--- 02_get_numbers_ticket.py
+++ 02_get_numbers_ticket.py
@@ -2,22 +2,15 @@
 # Генерувати набір унікальних випадкових сортованих чисел від 1 до 1000
 # min >= 1, max <= 1000, min <= quantity <= max
 #------------------------------------------------------------------------------
-# трохи незрозуміла умова min <= quantity <= max - чому кількість варіантів
-# не може бути менше min?
-# взагалі кількість унікальних варіантів може бути менше або дорівнює
-# max - min + 1
 
 def get_numbers_ticket(min, max, quantity):
     try:
-        if int(min) != min or int(max) != max or int(quantity) != quantity or min < 1 or max > 1000 or quantity > max - min + 1:
+        if type(min) != int or type(max) != int or type(quantity) != int or min < 1 or max > 1000 or quantity > max - min + 1:
             return []
     except ValueError:
         return []
-    nums = []
-    for i in range(max - min + 1):
-        nums.append(i + min)
     import random
-    lot = random.sample(nums, quantity)
+    lot = random.sample(range(min, max + 1), quantity)
     lot.sort()
     return lot
 
